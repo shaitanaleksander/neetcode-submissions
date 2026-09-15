293e6245e70987728be49847e39090616d4ef6a4
Oleksandr Shaitan <shaitanaleksander@gmail.com>
2026-09-15 13:22:50 +0200

    Add: divide-array-into-equal-pairs - submission-10

---
+++ Data Structures & Algorithms/divide-array-into-equal-pairs/submission-10.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        
+        int [] arr = new int [501];
+
+        for(int i: nums) arr[i]++;
+
+       
+
+        for(int i: arr) {
+            if (i !=0 && i % 2 != 0) return false;
+        }
+        return true;
+
+
+    }
+}
